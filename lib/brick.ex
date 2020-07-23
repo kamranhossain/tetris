36061c89803157e3d34e1b846d586889357e824c
kamranhossain <kamran.hossain227@gmail.com>
2020-07-23 15:56:20 +0600

    add new_random function with changing value of struct

--- lib/brick.ex
+++ lib/brick.ex
@@ -6,4 +6,13 @@ defmodule Tetris.Brick do
 
   def new(), do: __struct__()
 
+  def new_random() do
+    %{
+      name: random_name(),
+      location: {40, 0},
+      rotation: random_rotation(),
+      reflection: random_reflection()
+    }
+  end
+
 end
