478dd5dc74471afb8dca40f65bd828a0a0265cef
kamranhossain <kamran.hossain227@gmail.com>
2020-07-27 21:08:33 +0600

    add a private function transpose  to transpose the points of the shape

--- lib/points.ex
+++ lib/points.ex
@@ -2,4 +2,10 @@ defmodule Tetris.Points do
   def translate(points, {x, y}) do
     Enum.map(points, fn {dx, dy} -> {dx + x, dy + y} end)
   end
+
+  defp transpose(points) do
+    points
+    |> Enum.map(fn {x, y} -> {y, x} end)
+  end
+
 end
